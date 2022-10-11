6ab27c4e1e26f1ad6b312a6698cc1091bf97a501
Bruce Han <intrager@naver.com>
2022-10-11 16:26:32 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 128 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -9,7 +9,6 @@ public class Main {
         N = scan.nextInt();
         nums = new int[N + 1]; // N 길이의 배열 초기화
         operators = new int[5]; // 연산자는 4가지(총 길이 5개)
-        order = new int[N + 1]; // 기록을 저장할 배열
         for(int i = 1; i <= N; i++) nums[i] = scan.nextInt();
         for(int i = 1; i <= 4; i++) operators[i] = scan.nextInt();
         
@@ -19,29 +18,25 @@ public class Main {
     }
 
     static int N, max, min;
-    static int[] nums, operators, order;
+    static int[] nums, operators;
     
-    // 정한 연산자 순서대로(완성된 식에 맞게) 계산해서 정답을 갱신하기
-    static int calculator() {
-        // nums, order
-        int value = nums[1]; //처음 들고있는 숫자는 첫 번째 숫자
-        for(int i = 1; i <= N - 1; i++) {
-            // value order[i] nums[i+1]
-            if(order[i] == 1) // +
-                value = value + nums[i + 1];
-            if(order[i] == 2) // -
-                value = value - nums[i + 1];
-            if(order[i] == 3) // *
-                value = value * nums[i + 1];
-            if(order[i] == 4) // /
-                value = value / nums[i + 1];
-        }
-        return value;
+    // 피연산자 2개와 연산자 1개가 주어졌을 때 계산해주는 함수
+    static int calculator(int operand1, int operator, int operand2) {
+        // value order[i] nums[i+1]
+        if(operator == 1) // +
+            return operand1 + operand2;
+        else if(operator == 2) // -
+            return operand1 - operand2;
+        else if(operator == 3) // *
+            return operand1 * operand2;
+        else // /
+            return operand1 / operand2;
     }
     // order[1...N-1]에 연산자들이 순서대로 저장될 것이다.
-    static void recFunc(int k) {
+    // k - 1번째 연산자까지 계산한 결과가 value임
+    static void recFunc(int k, int value) {
         if(k == N) {    // 모든 연산자들을 전부 나열하는 방법을 찾은 상태
-            int value = calculator();
+            // value를 정답에 갱신
             max = Math.max(max, value);
             min = Math.min(min, value);
         } else {    // k 번째 연산자는 무엇을 선택할 것인가?
@@ -49,10 +44,9 @@ public class Main {
             for(int cand = 1; cand <= 4; cand++) {
                 if(operators[cand] >= 1) { // 해당 사용될 4칙연산이 최소 1개 있으면
                     operators[cand]--;    // 사용했다를 기록
-                    order[k] = cand; // order에다가 cand번째 4칙연산을 썼다
-                    recFunc(k+1);
+                    // 지금의 중간계산값과 연산자, 다음(새로운) 숫자
+                    recFunc(k + 1, calculator(value, cand, nums[k+1]));
                     operators[cand]++; // cand번 사용된 연산자의 횟수 다시 복구(사용했다는 사실 취소)
-                    order[k] = 0;
                 }
             }
         }
@@ -61,7 +55,7 @@ public class Main {
     public static void main(String[] args) {
         input();
         // 1번째 원소부터 M번째 원소를 조건에 맞게 고르는 모든 방법 탐색
-        recFunc(1);
+        recFunc(1, nums[1]); // 첫 번째 원소의 계산 결과는 어차피 1번지의 원소값
         sb.append(max).append('\n').append(min);
         System.out.println(sb.toString());
     }
@@ -111,7 +105,4 @@ public class Main {
             return str;
         }
     } 
-    
-    
-    
 }
