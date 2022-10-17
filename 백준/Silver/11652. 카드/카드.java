c332f189e3f05e491a960949992bdd82d2a81dde
Bruce Han <intrager@naver.com>
2022-10-17 21:31:03 +0900

    [Silver IV] Title: 카드, Time: 400 ms, Memory: 33388 KB -BaekjoonHub

---
+++ 백준/Silver/11652. 카드/카드.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    
+    static int N;
+    static long[] a;
+    
+    static void input() {
+        N = scan.nextInt();
+        a = new long[N+1];
+        for(int i = 1; i <= N; i++) {
+            a[i] = scan.nextLong();
+        }
+    }
+    
+    static void pro() {
+        // Sort 정렬하기
+        Arrays.sort(a, 1, N+1);
+        
+        // mode : 최빈값, modeCnt : 최빈값의 등장 횟수, curCnt : 현재 값(a[1])의 등장 횟수
+        int modeCnt = 1, curCnt = 1;
+        long mode = a[1];
+        
+        // 2번 원소부터 차례대로 보면서, 같은 숫자가 이어서 나오고 있는지, 새로운 숫자가 나왔는지 판단하여
+        // curCnt를 갱신해주고, 최빈값을 갱신하는 작업
+        for(int i = 2; i <= N; i++) {
+            if(a[i] == a[i-1]) { // 지금 보고 있는 숫자가 바로 앞의 숫자와 같다면
+                curCnt++;
+            } else {
+                curCnt = 1;
+            }
+            if(mode == a[i]) modeCnt++;
+            if(modeCnt < curCnt) {
+                modeCnt = curCnt;
+                mode = a[i];
+            }
+        }
+        System.out.println(mode);
+    }
+    
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+    
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+        
+        String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+        
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
