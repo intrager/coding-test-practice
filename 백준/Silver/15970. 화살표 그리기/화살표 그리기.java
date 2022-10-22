2a65ca90e0fb5df581370e19b1d23d4b25efe49a
Bruce Han <intrager@naver.com>
2022-10-22 13:58:55 +0900

    [Silver IV] Title: 화살표 그리기, Time: 200 ms, Memory: 16500 KB -BaekjoonHub

--- 백준/Silver/15970. 화살표 그리기/화살표 그리기.java
+++ 백준/Silver/15970. 화살표 그리기/화살표 그리기.java
@@ -6,49 +6,46 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
     
     static int N;
-    static ArrayList<Integer>[] a; // 색깔마다 점을 모을 ArrayList배열
+    static ArrayList<Integer>[] a;
     
     static void input() {
         N = scan.nextInt();
-        a = new ArrayList[N+1];
+        a = new ArrayList[N + 1];
         for(int color = 1; color <= N; color++) {
             a[color] = new ArrayList<Integer>();
-        } // 입력받음
+        }
         for(int i = 1; i <= N; i++) {
             int coord, color;
             coord = scan.nextInt();
             color = scan.nextInt();
-            a[color].add(coord); // a[color]에다가 coord라는 점의 위치가 있더라
+            a[color].add(coord);
         }
     }
     
     static int toLeft(int color, int idx) {
-        // 색깔이 color인 점의 idx번째에 있는 점이 왼쪽으로 화살표를 그린다면
-        // 화살표의 길이를 return하는 함수. 왼쪽에 점이 없다면 무한대를 return
-        if(idx == 0) return Integer.MAX_VALUE;
+        if(idx == 0) // 왼쪽에 더이상 점이 없는 상태
+            return Integer.MAX_VALUE;
         return a[color].get(idx) - a[color].get(idx - 1);
     }
     
     static int toRight(int color, int idx) {
-        // 색깔이 color인 점의 idx번째에 있는 점이 오른쪽으로 화살표를 그린다면
-        // 화살표의 길이를 return하는 함수. 오른쪽에 점이 없다면 무한대를 return
-        if(idx + 1== a[color].size()) return Integer.MAX_VALUE; // 오른쪽에 있는 점이 color를 갖는 색의 개수와 동일하다면 
-        return a[color].get(idx + 1) - a[color].get(idx); //idx+1번지 값에서 자신의 값을 뺌
+        if(idx + 1 == a[color].size()) // 오른쪽 점 + 1이 color를 갖는 색의 개수(배열의 크기)와 같다면
+            return Integer.MAX_VALUE;
+        return a[color].get(idx + 1) - a[color].get(idx);
     }
     
     static void pro() {
+        for(int color = 1; color <= N; color++)
+            Collections.sort(a[color]); // ArrayList, a의 color번지에 있는 배열을 정렬해달라
+        
+        int ans = 0;
         for(int color = 1; color <= N; color++) {
-            Collections.sort(a[color]); // 오름차순 정렬
-        }
-        int ans = 0; // 정답 카운트
-        for(int color = 1; color <= N; color++) {
-            for(int i = 0; i < a[color].size(); i++) { // 왼/오 중에 가까운 게 뭐냐
-                int left = toLeft(color, i); //지금 보고 있는 색에 지금 인덱스 값
+            for(int i = 0; i < a[color].size(); i++) {
+                int left = toLeft(color, i);
                 int right = toRight(color, i);
-                ans += Math.min(left, right);
+                ans += Math.min(left, right); // ans에는 둘 중 가까운 거리를 넣어야 함
             }
         }
-        // 정답출력
         System.out.println(ans);
     }
     
