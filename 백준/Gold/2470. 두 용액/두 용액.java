41ad6ffeb25b045a43bbfc9708c1cd71f67283ec
Bruce Han <intrager@naver.com>
2022-10-24 01:11:12 +0900

    [Gold V] Title: 두 용액, Time: 404 ms, Memory: 28276 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -17,44 +17,46 @@ public class Main {
     }
     
     static int lower_bound(int[] A, int L, int R, int X) {
-        // A[L...R] 에서 X 이상의 수 중 제일 왼쪽 인덱스를 return 하는 함수
-        // 그런 게 없다면 R + 1을 return 한다
-        int result = R + 1;
+        // A[L...R]에서 X 이상의 수 중 제일 왼쪽 인덱스를 return 하는 함수
+        // 그런 게 없다면 R + 1을 return한다
+        int res = R + 1; // 만약 A[L...R]중
         while(L <= R) {
             int mid = (L + R) / 2;
             if(A[mid] >= X) {
-                result = mid;
+                res = mid;
                 R = mid - 1;
             } else {
                 L = mid + 1;
             }
         }
-        return result;
+        return res;
     }
     
     static void pro() {
-        // A에 대해 이분 탐색을 할 예정이므로 정렬을 미리 해둠
+        // A에 대해 이분 탐색을 할 예정이니까 정렬을 미리 함
         Arrays.sort(A, 1, N + 1);
         
         int best_sum = Integer.MAX_VALUE;
         int v1 = 0, v2 = 0;
-        for(int left = 1; left < N; left++) {
-            // A[left] 용액을 쓸 것이다. 고로 -A[left]와 가장 가까운 용액을 자신의 오른쪽 구간에서 찾자.
-            int result = lower_bound(A, left + 1, N, -A[left]);
+        for(int left = 1; left <= N - 1; left++) {
+            // A[left] 용액을 쓸 것이므로 -A[left]와 가장 가까운 용액을 자신의 오른쪽 구간에서 찾을 것
+            int res = lower_bound(A, left + 1, N, -A[left]);
             
-            // A[result - 1]와 A[result] 중에 A[left]와 섞었을 때의 정보를 정답에 갱신시킨다.
+            // A[res - 1]와 A[res] 중에 A[left]와 섞엇을 때 정보를 정답에 갱신시킨다.
             
-            // 내가 진짜 원하는 범위냐? && 이 두개의 합 절대값이 best_sum을 갱신할 수 있냐?
-            if(left + 1 <=  result - 1 && result - 1 <= N && Math.abs(A[result - 1] + A[left]) < best_sum) {
-                best_sum = Math.abs(A[left] + A[result - 1]);
+            // 1. A[left] + A[res - 1]
+            if(left < res - 1 && Math.abs(A[left] + A[res - 1]) < best_sum) {
+                best_sum = Math.abs(A[left] + A[res - 1]);
                 v1 = A[left];
-                v2 = A[result - 1];
-            } // result - 1
-            if(left + 1 <= result && result <= N && Math.abs(A[result] + A[left]) < best_sum) {
-                best_sum = Math.abs(A[left] + A[result]);
+                v2 = A[res - 1];
+            }
+            
+            // 2. A[left] + A[res]
+            if(res <= N && Math.abs(A[left] + A[res]) < best_sum) {
+                best_sum = Math.abs(A[left] + A[res]);
                 v1 = A[left];
-                v2 = A[result];
-            } // result
+                v2 = A[res];
+            }
         }
         sb.append(v1).append(' ').append(v2);
         System.out.println(sb);
