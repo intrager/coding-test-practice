3a374a7bc89a8e01f454917d1f210bc635137a2a
Bruce Han <intragerhan@gmail.com>
2022-12-02 21:41:50 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.35 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,10 +1,7 @@
+import java.util.Arrays;
 class Solution {
     public int solution(int[] numbers) {
-        int answer = Integer.MIN_VALUE;
-        for(int i = 0; i < numbers.length; i++) {
-            for(int j = i + 1; j < numbers.length; j++)
-                answer = Math.max(answer, numbers[i] * numbers[j]);
-        }
-        return answer;
+        Arrays.sort(numbers);
+        return numbers[numbers.length-1] * numbers[numbers.length-2] < numbers[0] * numbers[1] ? numbers[0] * numbers[1] : numbers[numbers.length-1] * numbers[numbers.length-2];
     }
 }
