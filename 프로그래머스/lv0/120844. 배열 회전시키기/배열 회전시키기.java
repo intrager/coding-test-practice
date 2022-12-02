4d9d0bb3e47f6cb7da9221e94e8d779c5fc75702
Bruce Han <intragerhan@gmail.com>
2022-12-02 18:56:25 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.02 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -2,16 +2,14 @@ class Solution {
     public int[] solution(int[] numbers, String direction) {
         int len = numbers.length;
         int[] answer = new int[len];
-        if(direction.equals("right")) {
-            for(int i = 1; i < len; i++) {
-                answer[i] = numbers[i-1];
+        for(int i = 0; i < len; i++) {
+            if(direction.equals("right")) {
+                if(i < numbers.length - 1) answer[i + 1] = numbers[i];
+                else answer[0] = numbers[i];
+            } else {
+                if(i == 0) answer[len-1] = numbers[0];
+                else answer[i-1] = numbers[i];
             }
-            answer[0] = numbers[len-1];
-        } else if(direction.equals("left")) {
-            for(int i = 0; i < len-1; i++) {
-                answer[i] = numbers[i+1];
-            }
-            answer[len-1] = numbers[0];
         }
         return answer;
     }
