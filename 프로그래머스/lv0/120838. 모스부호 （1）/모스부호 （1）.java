0f7a1d17f9125b86b452180a79ef5bdf7541ede0
Bruce Han <intragerhan@gmail.com>
2022-12-03 23:54:11 +0900

    [level 0] Title: 모스부호 (1), Time: 0.26 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.java
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -1,17 +1,25 @@
 import java.util.Arrays;
+import java.util.HashMap;
 class Solution {
     public String solution(String letter) {
-        String[] morse = {".-", "-...", "-.-.", "-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        String morseStr = "'.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',"
+                + "'--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',"
+                + "'--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',"
+                + "'...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',"
+                + "'-.--':'y','--..':'z'";
         
-        String[] str = letter.split(" ");
-        String answer = "";
-        for(int i = 0; i < str.length; i++) {
-            for(int s = 0; s < morse.length; s++) {
-                if(str[i].equals(morse[s])) {
-                    answer += (char)(s + 97);
-                }
-            }
+        String[] morseArr = morseStr.split(",");
+        String[] letterArr = letter.split(" ");
+        
+        HashMap<String, String> morse = new HashMap<String, String>();
+        
+        for(int i = 0; i < morseArr.length; i++) {
+            morseArr[i] = morseArr[i].replace("'", "");
+            int idx = morseArr[i].indexOf(":");
+            morse.put(morseArr[i].substring(0, idx), morseArr[i].substring(idx+1));
         }
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < letterArr.length; i++) sb.append(morse.get(letterArr[i]));
+        return sb.toString();
     }
 }
