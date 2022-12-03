b5b34ff0a9866df4cc4b39a8dff58928d537557a
Bruce Han <intragerhan@gmail.com>
2022-12-03 13:06:52 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 5.72 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,9 +1,6 @@
+import java.util.Arrays;
 class Solution {
     public String solution(String[] seoul) {
-        int answer = 0;
-        for(int i = 0; i < seoul.length; i++)
-            if(seoul[i].equals("Kim")) 
-                answer = i;
-        return "김서방은 " + answer + "에 있다";
+        return "김서방은 " + Arrays.asList(seoul).indexOf("Kim") + "에 있다";
     }
 }
