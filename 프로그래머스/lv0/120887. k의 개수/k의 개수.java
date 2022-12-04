f6837650694639455fa7f410e616c2572da8cd5c
Bruce Han <intragerhan@gmail.com>
2022-12-04 11:46:42 +0900

    [level 0] Title: k의 개수, Time: 7.30 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+        for(int l = i; l <= j; l++) {
+ 
+            StringBuilder sb = new StringBuilder();
+            sb.append(l);
+            
+            int defaultLength = sb.length();
+            for(int r = 0; r < sb.length(); r++) {
+                if(sb.charAt(r) - '0' == k) answer++;
+            }
+        }
+        return answer;
+    }
+}
