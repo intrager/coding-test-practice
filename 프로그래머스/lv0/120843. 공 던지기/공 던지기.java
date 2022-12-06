3bb6e5c1cdfeedaaed30218515be6a84aaf50f47
Bruce Han <intragerhan@gmail.com>
2022-12-06 20:10:43 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
@@ -1,10 +1,5 @@
 class Solution {
     public int solution(int[] numbers, int k) {
-        int answer = 1;
-        for(int i = 0; i < k - 1; i++) {
-            answer += 2;
-            if(answer > numbers.length - 1) answer -= numbers.length;
-        }
-        return answer;
+        return numbers[2 * (k - 1) % numbers.length];
     }
 }
