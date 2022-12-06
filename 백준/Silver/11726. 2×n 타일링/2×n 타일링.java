515c878faac44fab77e83d4a089c4393e84db474
Bruce Han <intragerhan@gmail.com>
2022-12-06 17:05:50 +0900

    [Silver III] Title: 2×n 타일링, Time: 132 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+    
+    static int N;
+    static int[] Dy;
+    
+    static void input() {
+        N = scan.nextInt();
+    }
+    
+    static void pro() {
+        Dy = new int[1005];
+        Dy[1] = 1;
+        Dy[2] = 2;
+        
+        for(int i = 3; i <= N; i++) {
+            Dy[i] = (Dy[i-1] + Dy[i-2]) % 10007;
+        }
+        System.out.println(Dy[N]);
+    }
+    
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+    
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+        
+        String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+        
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
