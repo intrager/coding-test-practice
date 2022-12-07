f72fd79edfce05fac9d5184e3b6569b9559caa40
Bruce Han <intragerhan@gmail.com>
2022-12-07 14:21:03 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.50 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,14 +1,13 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-        StringBuffer sf = new StringBuffer();
-        s = s.toLowerCase();
-        sf.append(Character.toUpperCase(s.charAt(0)));
-        for(int i = 1; i < s.length(); i++) {
-            if(s.charAt(i) == ' ') sf.append(" ");
-            else if(s.charAt(i - 1) == ' ') sf.append(Character.toUpperCase(s.charAt(i)));
-            else sf.append(s.charAt(i));
+        String[] sp = s.toLowerCase().split("");
+        boolean flag = true;
+        
+        for(String ss : sp) {
+            answer += flag ? ss.toUpperCase() : ss;
+            flag = ss.equals(" ") ? true : false;
         }
-        return sf.toString();
+        return answer;
     }
 }
