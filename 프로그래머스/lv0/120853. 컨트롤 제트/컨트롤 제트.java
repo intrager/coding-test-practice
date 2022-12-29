9f6e490cc1ee0079e6df491395b3f5d3fcbbe57f
Bruce Han <intragerhan@gmail.com>
2022-12-29 23:48:23 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.19 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String s) {
+        String[] strArr = s.split(" ");
+        int answer = 0;
+        for(int i = 0; i < strArr.length; i++) {
+            if(strArr[i].equals("Z")) {
+                answer -= Integer.parseInt(strArr[i-1]);
+                continue;
+            } else {
+                answer += Integer.parseInt(strArr[i]);
+            }
+        }
+        return answer;
+    }
+}
