bf1d1ce6af964a8f47adbcfeca7a33c4ab29eda7
Bruce Han <intragerhan@gmail.com>
2023-01-13 16:05:21 +0900

    [level 0] Title: 문자열 밀기, Time: 0.05 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -2,22 +2,6 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(String A, String B) {
-        int answer = 0;
-        char[] arrA = A.toCharArray();
-        char[] arrB = B.toCharArray();
-        
-        int len = arrA.length;
-        for(int i = 0; i < len; i++) {
-            if(Arrays.equals(arrA, arrB)) {
-                return answer;
-            }
-            char temp = arrA[len-1];
-            for(int j = 1; j < len; j++) {
-                arrA[len-j] = arrA[len-j-1];
-            }
-            arrA[0] = temp;
-            answer++;
-        }
-        return -1;
+        return B.repeat(2).indexOf(A);
     }
 }
