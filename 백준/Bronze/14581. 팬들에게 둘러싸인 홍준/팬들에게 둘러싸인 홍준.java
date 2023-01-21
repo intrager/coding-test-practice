608c2f298adde6f8e1de92a2fb47bab2ce63fc30
Bruce Han <intragerhan@gmail.com>
2023-01-21 15:17:41 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 140 ms, Memory: 15916 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String hongZuneId = br.readLine();
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + hongZuneId + "::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
