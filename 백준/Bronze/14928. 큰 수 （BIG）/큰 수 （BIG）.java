07f1e0c34984b309b2086ffac9c371a2611841fa
Bruce Han <intragerhan@gmail.com>
2023-01-22 21:19:33 +0900

    [Bronze V] Title: 큰 수 (BIG), Time: 284 ms, Memory: 20132 KB -BaekjoonHub

---
+++ 백준/Bronze/14928. 큰 수 （BIG）/큰 수 （BIG）.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String N = br.readLine();
+        long remainder = 0;
+        
+        for(int i = 0; i < N.length(); i++) {
+            remainder = (remainder * 10 + (Character.getNumericValue(N.charAt(i)))) % 20000303;
+        }
+        
+        bw.write("" + remainder);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
