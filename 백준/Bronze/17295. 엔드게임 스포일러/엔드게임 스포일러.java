1702b56e64b46d5c817818041f9fa35a4fb82357
Bruce Han <intragerhan@gmail.com>
2023-01-24 01:32:10 +0900

    [Bronze V] Title: 엔드게임 스포일러, Time: 132 ms, Memory: 13932 KB -BaekjoonHub

---
+++ 백준/Bronze/17295. 엔드게임 스포일러/엔드게임 스포일러.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) throws java.io.IOException {
+        java.io.BufferedReader br = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
+        br.readLine();
+        System.out.println("Avengers: Endgame");
+    }
+}
