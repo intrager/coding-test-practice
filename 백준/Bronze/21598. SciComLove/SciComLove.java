351af19b48790e84738bb3f613b045691f1f0230
Bruce Han <intragerhan@gmail.com>
2023-01-25 17:48:42 +0900

    [Bronze V] Title: SciComLove, Time: 116 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        if(N % 2 == 0) {
+            for(int i = 0; i < 2; i++) System.out.println("SciComLove");
+        } else {
+            for(int i = 0; i < 1; i++) System.out.println("SciComLove");
+        }
+    }
+}
