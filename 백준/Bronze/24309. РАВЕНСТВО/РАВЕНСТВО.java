2331286ef5ac88554aac60579876a3f2f5d7704f
Bruce Han <intragerhan@gmail.com>
2023-01-27 18:29:55 +0900

    [Bronze V] Title: РАВЕНСТВО, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/24309. РАВЕНСТВО/РАВЕНСТВО.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        BigInteger a = new BigInteger(br.readLine());
+        BigInteger b = new BigInteger(br.readLine());
+        BigInteger c = new BigInteger(br.readLine());
+        
+        System.out.println((b.subtract(c)).divide(a));
+    }
+}
