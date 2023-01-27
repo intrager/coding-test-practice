ba11bafb34de46425df03d53ed88827573a79c0f
Bruce Han <intragerhan@gmail.com>
2023-01-28 00:04:47 +0900

    [Bronze V] Title: Pups, Time: 132 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Bronze/26575. Pups/Pups.java
+++ 백준/Bronze/26575. Pups/Pups.java
@@ -1,39 +1,30 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
+import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int n = Integer.parseInt(br.readLine());
-		
-		double dog = 0;
-		double food = 0;
-		double price = 0;
-		
-		StringTokenizer str;
-		double sum;
-		String sumStr;
-		for (int i = 0; i < n; i++) {
-			str = new StringTokenizer(br.readLine());
-			dog = Double.parseDouble(str.nextToken());
-			food = Double.parseDouble(str.nextToken());
-			price = Double.parseDouble(str.nextToken());
-			sum = dog*food*price;
-			sum *= 100;
-			sum = Math.round(sum) / 100.0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        
+        double d = 0;
+        double f = 0;
+        double p = 0;
+        double sum = 0;
+        
+        StringTokenizer st;
+        while(n-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            
+            d = Double.parseDouble(st.nextToken());
+            f = Double.parseDouble(st.nextToken());
+            p = Double.parseDouble(st.nextToken());
+            sum = d * f * p * 100;
             bw.write("$");
-			bw.write(String.format("%.2f", sum));
+            bw.write(String.format("%.2f",Math.round(sum) / 100.0));
             bw.newLine();
-		    bw.flush();
-		}
+            bw.flush();
+        }
         bw.close();
-	}
+    }
 }
