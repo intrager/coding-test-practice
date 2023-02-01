22288a19083c82e6048608301a2e8a3c18b693fc
Bruce Han <intragerhan@gmail.com>
2023-02-02 00:01:52 +0900

    [Bronze IV] Title: 입실 관리, Time: 128 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        while(N-- > 0) {
+            String name = br.readLine();
+            System.out.println(name.toLowerCase());
+        }
+    }
+}
