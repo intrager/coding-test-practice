787aa0cfc56a3407981b6e96446c266852564902
Bruce Han <intragerhan@gmail.com>
2023-02-26 21:12:30 +0900

    [Bronze IV] Title: 0123456789, Time: 212 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/6916. 0123456789/0123456789.java
@@ -0,0 +1,117 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        switch (a) {
+            case 0:
+                System.out.println(" * * *");
+                System.out.println("*     *");
+                System.out.println("*     *");
+                System.out.println("*     *");
+                System.out.println(" ");
+                System.out.println("*     *");
+                System.out.println("*     *");
+                System.out.println("*     *");
+                System.out.print(" * * *");
+                break;
+            case 1:
+                System.out.println(" ");
+                System.out.println("      *");
+                System.out.println("      *");
+                System.out.println("      *");
+                System.out.println(" ");
+                System.out.println("      *");
+                System.out.println("      *");
+                System.out.print("      *");
+                break;
+            case 2:
+                System.out.println(" * * *");
+                System.out.println("      *");
+                System.out.println("      *");
+                System.out.println("      *");
+                System.out.println(" * * *");
+                System.out.println("*");
+                System.out.println("*");
+                System.out.println("*");
+                System.out.print(" * * *");
+                break;
+            case 3:
+                System.out.println(" * * *");
+                System.out.println("      *");
+                System.out.println("      *");
+                System.out.println("      *");
+                System.out.println(" * * *");
+                System.out.println("      *");
+                System.out.println("      *");
+                System.out.println("      *");
+                System.out.print(" * * *");
+                break;
+            case 4:
+                System.out.println(" ");
+                System.out.println("*     *");
+                System.out.println("*     *");
+                System.out.println("*     *");
+                System.out.println(" * * *");
+                System.out.println("      *");
+                System.out.println("      *");
+                System.out.print("      *");
+                break;
+            case 5:
+                System.out.println(" * * *");
+                System.out.println("*");
+                System.out.println("*");
+                System.out.println("*");
+                System.out.println(" * * *");
+                System.out.println("      *");
+                System.out.println("      *");
+                System.out.println("      *");
+                System.out.print(" * * *");
+                break;
+            case 6:
+                System.out.println(" * * *");
+                System.out.println("*");
+                System.out.println("*");
+                System.out.println("*");
+                System.out.println(" * * *");
+                System.out.println("*     *");
+                System.out.println("*     *");
+                System.out.println("*     *");
+                System.out.print(" * * *");
+                break;
+            case 7:
+                System.out.println(" * * *");
+                System.out.println("      *");
+                System.out.println("      *");
+                System.out.println("      *");
+                System.out.println(" ");
+                System.out.println("      *");
+                System.out.println("      *");
+                System.out.print("      *");
+                break;
+            case 8:
+                System.out.println(" * * *");
+                System.out.println("*     *");
+                System.out.println("*     *");
+                System.out.println("*     *");
+                System.out.println(" * * *");
+                System.out.println("*     *");
+                System.out.println("*     *");
+                System.out.println("*     *");
+                System.out.print(" * * *");
+                break;
+            case 9:
+                System.out.println(" * * *");
+                System.out.println("*     *");
+                System.out.println("*     *");
+                System.out.println("*     *");
+                System.out.println(" * * *");
+                System.out.println("      *");
+                System.out.println("      *");
+                System.out.println("      *");
+                System.out.print(" * * *");
+                break;
+        }
+    }
+}
