dd8102c212d752a5c4f05d70e669bc76412ac830
Bruce Han <intragerhan@gmail.com>
2023-03-05 15:20:25 +0900

    [Bronze IV] Title: Rain Diary, Time: 128 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/27182. Rain Diary/Rain Diary.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        if(n < 8) {
+            System.out.print(m+7);
+        } else {
+            System.out.print(n-7);
+        }
+    }
+}
