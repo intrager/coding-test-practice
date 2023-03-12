5bbd91c21601bef1413be0e5474ecaa4401a2d82
Bruce Han <intragerhan@gmail.com>
2023-03-12 09:51:23 +0900

    [Bronze IV] Title: 팀 나누기, Time: 120 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/13866. 팀 나누기/팀 나누기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+    
+        System.out.print(Math.max(A+D, B+C) - Math.min(A+D, B+C));
+    }
+}
