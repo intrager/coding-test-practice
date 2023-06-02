89590dd6c9fb56986fa08b588c7b144abf511798
Bruce Han <intragerhan@gmail.com>
2023-06-02 21:31:05 +0900

    [unrated] Title: 수 조작하기 2, Time: 5.62 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int[] numLog) {
+        StringBuilder answer = new StringBuilder();
+        for(int i = 1; i < numLog.length; i++) {
+            switch(numLog[i] - numLog[i-1]) {
+                case 1   : answer.append("w"); break;
+                case -1  : answer.append("s"); break;
+                case 10  : answer.append("d"); break;
+                default : answer.append("a"); break;
+            }
+        }
+        return answer.toString();
+    }
+}
