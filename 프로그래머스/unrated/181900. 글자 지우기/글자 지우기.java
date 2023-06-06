a58eee2065d9e502311eb24461466f3277bd067c
Bruce Han <intragerhan@gmail.com>
2023-06-06 11:47:20 +0900

    [unrated] Title: 글자 지우기, Time: 0.09 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        StringBuilder sb = new StringBuilder(my_string);
+        
+        for(int i : indices) sb.setCharAt(i, ' ');
+        return sb.toString().replace(" ", "");
+    }
+}
