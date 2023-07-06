4d14297ab690117b09302b838980920ffe96918d
Bruce Han <intragerhan@gmail.com>
2023-07-06 22:26:32 +0900

    [level 1] Title: 삼총사, Time: 0.10 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        for(int i = 0; i < number.length; i++) {
+            for(int j = 0; j < number.length; j++) {
+                for(int k = 0; k < number.length; k++) {
+                    if(i != j && j != k && i != k) {
+                        if(number[i] + number[j] + number[k] == 0) answer++;
+                    }
+                }
+            }
+        }
+        return answer / 6;
+    }
+
+}
