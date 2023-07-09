8d6d230c91da000a25664754aefe838a13ee784c
Bruce Han <intragerhan@gmail.com>
2023-07-09 11:08:58 +0900

    [unrated] Title: 코드 처리하기, Time: 0.02 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String code) {
+        StringBuilder ret = new StringBuilder();
+        int mode = 0;
+        for(int idx = 0; idx < code.length(); idx++) {
+            if(mode == 0) {
+                if(code.charAt(idx) != '1' && idx % 2 == 0) ret.append(code.charAt(idx));
+                else if(code.charAt(idx) == '1') mode = 1;
+            } else {
+                if(code.charAt(idx) != '1' && idx % 2 == 1) ret.append(code.charAt(idx));
+                else if(code.charAt(idx) == '1') mode = 0;
+            }
+        }
+        return ret.toString().isEmpty() ? "EMPTY" : ret.toString();
+    }
+}
