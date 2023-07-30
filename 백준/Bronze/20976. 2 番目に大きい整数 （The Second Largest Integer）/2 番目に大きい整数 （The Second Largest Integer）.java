3e252f3ddc7e0923814a696f4970cea93ec78408
Bruce Han <intragerhan@gmail.com>
2023-07-30 10:59:22 +0900

    [Bronze IV] Title: 2 番目に大きい整数 (The Second Largest Integer), Time: 120 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/20976. 2 番目に大きい整数 （The Second Largest Integer）/2 番目に大きい整数 （The Second Largest Integer）.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] strArr = br.readLine().split(" ");
+        int[] arr = new int[3];
+        
+        for(int i = 0; i < 3; i++) arr[i] = Integer.parseInt(strArr[i]);
+        
+        Arrays.sort(arr);
+        System.out.print(arr[1]);
+    }
+}
