eade748a7bab8cd47b63c538d92a57e8d312f8db
Bruce Han <intragerhan@gmail.com>
2023-08-27 00:23:25 +0900

    [Bronze IV] Title: 帰省 (Homecoming), Time: 128 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/24072. 帰省 （Homecoming）/帰省 （Homecoming）.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        
+        if(A <= C && C < B) System.out.print(1);
+        else System.out.print(0);
+    }
+}
