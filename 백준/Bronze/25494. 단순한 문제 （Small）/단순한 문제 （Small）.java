3d2a7e9eb010059efbb7b9c595fcd998dce99026
Bruce Han <intragerhan@gmail.com>
2023-10-27 21:59:52 +0900

    [Bronze IV] Title: 단순한 문제 (Small), Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/25494. 단순한 문제 （Small）/단순한 문제 （Small）.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int T = Integer.parseInt(br.readLine());
+        while(T-- > 0) {
+            String[] arr = br.readLine().split(" ");
+            int a = Integer.parseInt(arr[0]);
+            int b = Integer.parseInt(arr[1]);
+            int c = Integer.parseInt(arr[2]);
+        
+            int min = Math.min(Math.min(a, b), c);
+            System.out.println(min);
+        }
+    }
+}
