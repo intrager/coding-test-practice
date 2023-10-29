5ce2b3992366c97afda7a6de923ba75ab43bf396
Bruce Han <intragerhan@gmail.com>
2023-10-29 12:53:11 +0900

    [Bronze IV] Title: ГРАДИНА, Time: 120 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/24294. ГРАДИНА/ГРАДИНА.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int w1 = Integer.parseInt(br.readLine());
+        int h1 = Integer.parseInt(br.readLine());
+        int w2 = Integer.parseInt(br.readLine());
+        int h2 = Integer.parseInt(br.readLine());
+    
+        System.out.print((h1 + h2) * 2 + (Math.max(w1, w2) * 2) + 4);
+    }
+}
