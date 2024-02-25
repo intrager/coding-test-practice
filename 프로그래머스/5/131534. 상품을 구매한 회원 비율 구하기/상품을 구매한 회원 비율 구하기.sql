7bf7f50dba55cfe872b89751933ba1205ca7b36d
Bruce Han <intragerhan@gmail.com>
2024-02-25 10:29:09 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,12 +1,15 @@
-select to_number(to_char(SALES_DATE, 'yyyy')) as year
-  , to_number(to_char(SALES_DATE, 'MM')) as month
-  , count(distinct user_id) as PUCHASED_USERS
-  , round(count(distinct user_id) / (select count(*)
-                                     from USER_INFO
-                                     where to_char(JOINED, 'yyyy') = '2021') , 1) as PUCHASED_RATIO
-from ONLINE_SALE
-where user_id in (select user_id
-                  from USER_INFO
-                  where to_char(JOINED, 'yyyy') = '2021')
-group by to_number(to_char(SALES_DATE, 'yyyy')), to_number(to_char(SALES_DATE, 'MM'))
-order by year, month
+select
+   to_char(SALES_DATE, 'yyyy') as YEAR
+ , to_number(to_char(sales_date, 'MM')) as MONTH
+ , count(distinct sale.user_id) as PUCHASED_USERS
+ , round(count(distinct sale.user_id) / (select count(user_id)
+                                        from user_info
+                                        where to_char(joined, 'yyyy') = '2021'), 1) as PUCHASED_RATIO 
+from online_sale sale
+where exists (
+              select 1
+              from user_info info
+              where info.user_id = sale.user_id
+                and to_char(info.joined, 'yyyy') = '2021'
+             )
+group by to_char(sales_date, 'yyyy'), to_number(to_char(sales_date, 'MM'))
