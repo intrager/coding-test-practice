379d6d8794cc1b927df9017e2c3c827401c34a5f
Bruce Han <intragerhan@gmail.com>
2024-04-28 12:44:37 +0900

    MySQL / NaNms

--- PROGRAMMERS/연도 별 평균 미세먼지 농도 조회하기.sql
+++ PROGRAMMERS/연도 별 평균 미세먼지 농도 조회하기.sql
@@ -1,4 +1,9 @@
-select year(YM) as 'YEAR', round(avg(PM_VAL1), 2) as PM10, round(avg(PM_VAL2), 2) as 'PM2.5'
+select 
+    -- DATE_FORMAT(YM, '%Y') AS YEAR,    -- 문자열 출력 (오답)
+    -- LEFT(YM, 4) AS YEAR,              -- 문자열 출력 (오답)
+    year(YM) as 'YEAR'                   -- 정수 출력 (정답)
+  , round(avg(PM_VAL1), 2) as PM10
+  , round(avg(PM_VAL2), 2) as 'PM2.5'
 from AIR_POLLUTION
 where LOCATION1 = '경기도'
   and LOCATION2 = '수원'
