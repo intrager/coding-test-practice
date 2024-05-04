bfc3b9411ff0ddc944a48ac6e127f2a1bf1abf2b
Bruce Han <intragerhan@gmail.com>
2024-05-04 16:50:58 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,23 +1,19 @@
 class Solution {
     public int solution(int storey) {
-        int answer = 0;
-        if(storey <= 1) return 1;
-        int rest;
-        while(storey != 0) {
-            rest = storey % 10;
+        if(storey == 0) 
+            return 0;
         
-            if(rest >= 6) {
-                storey += 10 - rest;
-                answer += 10 - rest;
-            } else if(rest == 5 && (storey / 10) % 10 >= 5) {
-                storey += 10 - rest;
-                answer += 10 - rest;
-            } else {
-                answer += rest;
-            }
-            storey /= 10;
-        }
+        if(storey % 10 == 0)
+            return solution(storey / 10);
         
-        return answer;
+        if(storey < 10)
+            return Math.min(storey, 11 - storey);
+        
+        int mod = storey % 10;
+        
+        int wayUp = 10 - mod + solution(storey + 10 - mod);
+        int wayDown = mod + solution(storey - mod);
+        
+        return Math.min(wayUp, wayDown);
     }
 }
