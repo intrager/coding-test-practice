957942b0b31ac1163c93c30b99814101a797592b
Bruce Han <intragerhan@gmail.com>
2024-05-07 22:33:29 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,17 +1,8 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        int size = brown + yellow;
-        for(int i = size; i > 0; i--) {
-            if(size % i == 0 && i >= size / i && size / i > 2) {
-                int l = i;
-                int h = size / i;
-                if(brown == (l + h) * 2 - 4) {
-                    answer[0] = l;
-                    answer[1] = h;
-                }
-            }
-        }
+        int a = (brown + 4) / 2;
+        int b = yellow + brown;
+        int[] answer = {(int)(a + Math.sqrt(a * a - 4 * b)) / 2, (int)(a - Math.sqrt(a * a - 4 * b)) / 2};
         return answer;
     }
 }
