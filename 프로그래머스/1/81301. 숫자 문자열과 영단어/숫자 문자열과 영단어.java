e7ed73708f48d80082cb3f5af8bd3bb498d3fe78
Bruce Han <intragerhan@gmail.com>
2024-05-08 22:32:07 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.31 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,40 +1,10 @@
 class Solution {
-    private String[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-    
     public int solution(String s) {
-        char[] arr = s.toCharArray();
-        StringBuffer sb = new StringBuffer();
-        String answer = "";
-        for(char ch : arr) {
-            char c = Character.toLowerCase(ch);
-            if(c < 97 || c > 122) {
-                sb = new StringBuffer();
-                answer += Character.getNumericValue(c);
-            } else {
-                sb.append(c);
-                if(isNumber(sb.toString())) {
-                    answer += convertToInt(sb.toString());
-                    sb = new StringBuffer();
-                }
-            }
-        }
-        return Integer.parseInt(answer);
-    }
-    
-    private boolean isNumber(String str) {
-        for(String alphabet : numbers) {
-            if(str.equals(alphabet))
-                return true;
-        }
-        return false;
-    }
-    
-    private int convertToInt(String str) {
+        String[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         for(int i = 0; i < numbers.length; i++) {
-            if(str.equals(numbers[i])) {
-                return i;
-            }
+            s = s.replaceAll(numbers[i], Integer.toString(i));
         }
-        return 0;
+        return Integer.parseInt(s);
     }
+
 }
