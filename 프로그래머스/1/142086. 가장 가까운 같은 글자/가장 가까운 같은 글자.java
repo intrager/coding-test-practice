fd4bcfb3eebb76d301a64f3ce7db171a4a46719e
Bruce Han <intragerhan@gmail.com>
2024-05-23 19:48:00 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 42.63 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,19 +1,8 @@
 import java.util.HashMap;
-import java.util.Map;
-
+import java.util.stream.IntStream;
+    
 class Solution {
     public int[] solution(String s) {
-        int len = s.length();
-        int[] answer = new int[len];
-        
-        for(int i = 0; i < len; i++) {
-            String str = s.substring(0, i);
-            if(str.indexOf(s.charAt(i)) == -1) {
-                answer[i] = -1;
-            } else {
-                answer[i] = i - str.lastIndexOf(s.charAt(i));
-            }
-        }
-        return answer;
+        return IntStream.range(0, s.length()).map(i -> s.substring(0, i).lastIndexOf(s.charAt(i)) > -1 ? i - s.substring(0, i).lastIndexOf(s.charAt(i)) : -1).toArray();
     }
 }
