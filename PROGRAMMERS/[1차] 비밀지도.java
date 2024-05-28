1a4e9ac43bc1ae144d522abaf756d52016fcee47
Bruce Han <intragerhan@gmail.com>
2024-05-28 16:33:53 +0900

    Java / 8.778749999999999ms

--- PROGRAMMERS/[1차] 비밀지도.java
+++ PROGRAMMERS/[1차] 비밀지도.java
@@ -1,51 +1,16 @@
+}
 class Solution {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-​
-        for(int i=0; i<arr1.length; i++){
-            String str = Integer.toBinaryString(arr1[i]);
-​
-            for(int j=0; j<str.length(); j++){
-                if(answer[i] == null) answer[i] = "";
-​
-                if(str.charAt(j) == '1'){
-                    answer[i] += "#";
-                }else{
-                    answer[i] += " ";
-                }
-            }
-        }
-​
-        for(int i=0; i<arr2.length; i++){
-            String str = "";
-            String ans = "";
-​
-            if(answer[i].length() < n){
-                for(int k=0; k<n-answer[i].length(); k++){
-                    ans += " ";
-                }
-            }
-​
-            if((Integer.toBinaryString(arr2[i])).length() < n){
-                for(int k=0; k<n-(Integer.toBinaryString(arr2[i])).length(); k++){
-                    str += " ";
-                }
-            }
-​
-            ans += answer[i];
-            answer[i] = "";
-            str += Integer.toBinaryString(arr2[i]);
-​
-            for(int j=0; j<str.length(); j++){
-                if((str.charAt(j) == '1') || (ans.charAt(j) != ' ')){
-                    answer[i] += "#";
-                }else{
-                    answer[i] += " ";
-                }
-            }
-        }
-​
-​
-        return answer;
-    }
+  public String[] solution(int n, int[] arr1, int[] arr2) {
+      String[] result = new String[n];
+      for(int i = 0; i < n; i++) {
+          result[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+      }
+      
+      for(int i = 0; i < n; i++) {
+          result[i] = String.format("%" + n + "s", result[i]);
+          result[i] = result[i].replaceAll("1", "#");
+          result[i] = result[i].replaceAll("0", " ");
+      }
+      return result;
+  }
 }
