5ac36f1b37c8c63924bd2bb09285cdcec2078640
Bruce Han <intragerhan@gmail.com>
2024-05-29 23:30:14 +0900

    [level 1] Title: K번째수, Time: 0.52 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,10 +3,7 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         for(int i = 0; i < commands.length; i++) {
-            int[] arr = new int[commands[i][1] - commands[i][0] + 1];
-            for(int j = commands[i][0] - 1; j < commands[i][1]; j++) {
-                arr[j - commands[i][0] + 1] = array[j];
-            }
+            int[] arr = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
             Arrays.sort(arr);
             answer[i] = arr[commands[i][2] - 1];
         }
